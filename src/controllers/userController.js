9f9eb871fed194df14c97690fa71efa250c285ee
DEEPAK CHAUHAN <chauhandeepak9912@gmail.com>
2024-10-28 09:27:37 +0530

    Added create uer API

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,29 @@
 const { model } = require("mongoose");
-function createUser() {
-  console.log(" user Controller called");
+const UserService = require("../services/userService");
+const UserRepository = require("../repos/userRepository");
+async function createUser(req, res) {
+  console.log("create user Controller called");
+  console.log(req.body);
+  //TODO- Register the user
+
+  const userService = new UserService(new UserRepository());
+  console.log(userService);
+
+  try {
+    const response = await userService.registerUser(req.body);
+
+    return res.json({
+      message: "Successfully registered the user",
+      success: true,
+      data: response,
+    });
+  } catch (error) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.reason,
+      data: {},
+      error: error,
+    });
+  }
 }
 module.exports = { createUser };
