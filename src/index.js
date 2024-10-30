1b9cbf2ef1bf8bb5d563848cd041894e6b27ec8a
DEEPAK CHAUHAN <chauhandeepak9912@gmail.com>
2024-10-30 09:55:07 +0530

    isLoggedIn middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 // const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 const ServerConfig = require("./config/serverConfig");
 const connectDB = require("./config/dbConfig");
@@ -7,16 +8,25 @@ const User = require("./schema/userSchema");
 const userRouter = require("./routes/userRoute");
 const cartRouter = require("./routes/cartRoute");
 const authRouter = require("./routes/authRoute");
+const { isLoggedIn } = require("./validation/authValidator");
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/users", userRouter); //connects the user router to the server
+//All my routes....
+app.use("/users", userRouter);
 app.use("/carts", cartRouter);
 app.use("/auth", authRouter);
+
+app.get("/ping", isLoggedIn, (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  return res.json({ message: "pong" });
+});
 
 app.listen(ServerConfig.PORT, async () => {
   await connectDB();
